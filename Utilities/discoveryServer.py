40f453cdf4a437b65ddd3f2b72c232c35296e282
Y S <yehonatan349@gmail.com>
2022-03-01 10:55:16 +0200

    Change to TCP

--- Utilities/discoveryServer.py
+++ Utilities/discoveryServer.py
@@ -1,15 +1,21 @@
+import socket
 from server import Server
+from configparser import ConfigParser
 
-class discoveryServer(Server):
+
+class discoveryServer(Server(6051,socket.SOCK_DGRAM)): #TODO: Change to UDP - Done.
   
+    def __init__(self):
+            ipConfig = ConfigParser()
+            ipConfig.read('./Utilities/IPconfig.ini')
+            self.__HOST = ipConfig['IP']['HOST']
+            self.__PORT =ipConfig['IP']['PORT']
 
-     def _Server__handle_client(self, client):
-        client.send('IP'.encode(self.FORMAT))
-        if client.recv(1024).decode(self.FORMAT) == 'send':
-            client.send('127.0.0.1'.encode(self.FORMAT))
+    def _Server__handle_client(self, client):
+        client.send(f'{self.__HOST}:{self.__PORT}'.encode(self.__FORMAT)) #TODO: move the IP to a configuration file - Done.
         client.close()
 
 
 if __name__ == "__main__":
-    s = discoveryServer()
+    s = discoveryServer(6050)
     s.startServer()
